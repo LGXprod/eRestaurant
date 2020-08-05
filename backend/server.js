aa66dc149319422d77b47bb0314bdf929f7370fe
LGXprod <mattghanno@gmail.com>
2020-08-05 11:38:08 +1000

    UPDATE configured bodyParser and required loginController

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+
+const restaurantApp = express();
+restaurantApp.use(bodyParser.urlencoded({ extended: true }));
+
+const loginController = require("./controllers/loginController");
+
+restaurantApp.listen(5000, function() {
+    console.log("Server running on port 5000");
+});
+
+const mongoURI = `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.bf9ms.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+(async () => {
+    await mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+    loginController(restaurantApp);
+})();
