81195dad2491bbefd504c521eab2f866b3d375e2
LGXprod <mattghanno@gmail.com>
2020-08-09 03:19:04 +1000

    ADD post request to send login details and to handle the response from the server. If successful creates a cookie that contains session data

--- erestaurant-app/src/routes/Home/components/LoginForm.js
+++ erestaurant-app/src/routes/Home/components/LoginForm.js
@@ -1,18 +1,69 @@
-import React from "react";
-import { Grid, Paper, Typography, Container, TextField, Button, makeStyles } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import {
+  Grid,
+  Paper,
+  Typography,
+  Container,
+  TextField,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import queryString from "querystring";
+import { Redirect } from "react-router-dom";
+import Cookies from "universal-cookie";
 
 const useStyles = makeStyles((theme) => ({
   formRows: {
     margin: theme.spacing(1),
-    textAlign: "center"
+    textAlign: "center",
   },
   formHeading: {
-    color: "#364f6b"
-  }
+    color: "#364f6b",
+  },
 }));
+
+const cookies = new Cookies();
 
 const LoginForm = () => {
   const classes = useStyles();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isUser, setIsUser] = useState(null);
+
+  useEffect(() => {
+    console.log("x", isUser);
+  }, [isUser]);
+
+  // setIsUser(null)
+
+  async function checkLogin(username, password) {
+    const response = await fetch("/Login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+      body: queryString.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+
+    response.json().then(res => {
+      if (res.auth) {
+        const expiryDate = new Date();
+        expiryDate.setMinutes(expiryDate.getMinutes() + 30);
+        console.log(expiryDate)
+
+        cookies.set("Session id", res.session_id, {
+          expires: expiryDate
+        });
+        setIsUser(true);
+      } else {
+        setIsUser(false);
+      }
+    }).catch(err => console.log(err));
+  }
 
   return (
     <React.Fragment>
@@ -24,15 +75,48 @@ const LoginForm = () => {
             justify="center"
             alignItems="center"
           >
-            <Typography className={`${classes.formRows} ${classes.formHeading}`} variant="h3">eRestaurant Login</Typography>
-            <TextField className={classes.formRows} id="outlined-basic" label="Username" variant="outlined" />
-            <TextField className={classes.formRows} id="outlined-basic" label="Password" variant="outlined" />
-            <Button className={classes.formRows} variant="contained">Submit</Button>
+            <Typography
+              className={`${classes.formRows} ${classes.formHeading}`}
+              variant="h3"
+            >
+              eRestaurant Login
+            </Typography>
+
+            <TextField
+              className={classes.formRows}
+              id="outlined-basic"
+              label="Username"
+              variant="outlined"
+              onChange={(event) => setUsername(event.target.value)}
+            />
+
+            <TextField
+              className={classes.formRows}
+              id="outlined-basic"
+              label="Password"
+              type="password"
+              variant="outlined"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+
+            {isUser === null ? null : isUser ? (
+              <Redirect to="/Dashboard" />
+            ) : (
+              <Typography variant="caption">Incorrect username or password.</Typography>
+            )}
+
+            <Button
+              className={classes.formRows}
+              variant="contained"
+              onClick={() => checkLogin(username, password)}
+            >
+              Submit
+            </Button>
           </Grid>
         </Paper>
       </Container>
     </React.Fragment>
   );
-}
+};
 
 export default LoginForm;
