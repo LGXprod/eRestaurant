d926237acec7118bfa4f14a85570f4245463996f
LGXprod <mattghanno@gmail.com>
2020-08-09 03:22:17 +1000

    UPDATE sends response to front end which includes the session id if login is successful

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,9 +1,27 @@
 const customer = require("../models/customer");
+const session = require("../models/session");
 
 module.exports = (app) => {
-    app.post("/login", (req, res) => {
-        customer.checkCustomerLogin(req.body.username, req.body.password).then((isCustomer) => {
-            console.log("Correct login:", isCustomer);
-        }).catch(err => console.log(err));
-    });
-}
+  app.post("/Login", (req, res) => {
+    customer
+      .checkCustomerLogin(req.body.username, req.body.password)
+      .then((correctLogin) => {
+        if (correctLogin) {
+          session
+            .createSession(req.body.username)
+            .then((session_id) => {
+              res.json({
+                auth: true,
+                session_id: session_id,
+              });
+            })
+            .catch((err) => console.log(err));
+        } else {
+          res.json({
+            auth: false,
+          });
+        }
+      })
+      .catch((err) => console.log(err));
+  });
+};
