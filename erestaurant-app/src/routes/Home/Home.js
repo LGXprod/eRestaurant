43d8c3279fd4122bf033dd4ae4a38f6e2d748cca
MichaelLarsen28 <62678953+MichaelLarsen28@users.noreply.github.com>
2020-08-18 11:54:41 +1000

    added Header component which uses appbar from materialui

--- erestaurant-app/src/routes/Home/Home.js
+++ erestaurant-app/src/routes/Home/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Helmet } from 'react-helmet';
 import LoginForm from "./components/LoginForm";
+import Header from "./components/Header";
+
 
 function Home() {
   return (
     <div className="Home">
       <Helmet>
-      <title>eRestaurant</title>
+      <title>Login to your account at DineOut</title>
       </Helmet>
+      <Header/>
       <LoginForm />
     </div>
   );
