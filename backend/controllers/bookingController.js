8cd91b8f63de77f56e489b459c279d202645e509
Hannah van Roy <hannahvanroy@MacBook-Pro-4.local>
2020-08-27 18:42:36 +1000

    UPDATE testable table availability

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,10 +1,14 @@
 const customer = require("../models/customer");
 const session = require("../models/session");
+const table = require("../models/table");
 
 module.exports = (app) => {
   app.post("/Booking", (req, res) => {
-
-    //TODO//
-    
+    res.send("hello");
+    table
+      .findAvailableTables((tables) => {
+        console.log(tables);
+      })
+      .catch((err) => console.log(err));
   });
 };
