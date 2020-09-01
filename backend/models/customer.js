6a88d8f20bf4a78c650d21570bf92891eb559047
Matthew <mattghanno@gmail.com>
2020-09-01 22:21:53 +1000

    ADD verification of form data and if valid adds new customer to customer collection

--- backend/models/customer.js
+++ backend/models/customer.js
@@ -1,39 +1,77 @@
 const mongoose = require("mongoose");
 
 const customerSchema = new mongoose.Schema({
-    _id: String,
-    password: String
+  _id: String,
+  password: String,
 });
 
 const Customer = mongoose.model("Customer", customerSchema);
 
 const createNewCustomer = (formData) => {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    let hasInvalidProp = false;
 
-    });
-}
+    const requiredProps = {
+      username: false,
+      password: false,
+      fName: false,
+      sName: false,
+      email: false,
+      mobileNum: false,
+      role: false
+    };
+
+    // check props are valid
+    for (let prop in formData) {
+      if (requiredProps[prop] != null) {
+        requiredProps[prop] = true;
+      } else {
+        hasInvalidProp = true;
+        break;
+      }
+    }
+
+    // check props aren't missing
+    if (hasInvalidProp) {
+      reject("Invalid props");
+    } else {
+      for (let prop in requiredProps) {
+        if (requiredProps[prop]) {
+          const customer = new Customer(formData);
+
+          customer.save(err => {
+            if (err) reject(err);
+            resolve();
+          });
+        } else {
+          reject("Missing props");
+        }
+      }
+    }
+  });
+};
 
 const checkCustomerLogin = (username, password) => {
-    return new Promise((resolve, reject) => {
-        Customer.findById(username, function(err, customer) {
-            if (err) reject(err);
+  return new Promise((resolve, reject) => {
+    Customer.findById(username, function (err, customer) {
+      if (err) reject(err);
 
-            console.log(customer);
-            
-            if (customer == null) {
-                resolve(false)
-            } else {
-                if (customer.password === password) {
-                    resolve(true);
-                } else {
-                    resolve(false);
-                }
-            }
-        });
+      console.log(customer);
+
+      if (customer == null) {
+        resolve(false);
+      } else {
+        if (customer.password === password) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      }
     });
-}
+  });
+};
 
-module.exports= {
-    createNewCustomer: createNewCustomer,
-    checkCustomerLogin: checkCustomerLogin
-}
+module.exports = {
+  createNewCustomer: createNewCustomer,
+  checkCustomerLogin: checkCustomerLogin,
+};
