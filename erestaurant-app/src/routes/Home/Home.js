caed9b3f566fbe3b524fe4687a8bb273df69c8c8
Matthew <mattghanno@gmail.com>
2020-09-01 14:04:14 +1000

    UPDATE Home and and LoginForm now use react context api so that the sign up link in the form causes the login to be hidden and the registration to be displayed

--- erestaurant-app/src/routes/Home/Home.js
+++ erestaurant-app/src/routes/Home/Home.js
@@ -1,39 +1,44 @@
-import React, { useState } from "react";
-import {
-  Button,
-  withStyles,
-} from "@material-ui/core";
+import React, { useState, createContext } from "react";
+import { Button, withStyles } from "@material-ui/core";
 import { Helmet } from "react-helmet";
 import LoginForm from "./components/LoginForm";
 import RegForm from "./components/RegForm";
 import Styles from "./Styles";
+import HomeContext from "./HomeContext";
 
 function Home(props) {
   const { classes } = props;
   const [displaySignIn, setDisplaySignIn] = useState(true);
 
+  function switchMethod() {
+    setDisplaySignIn((displaySignIn) =>
+      displaySignIn ? !displaySignIn : displaySignIn
+    );
+  }
+
   return (
-    <div className="Home">
-      <Helmet>
-        <title>Login to your account at DineOut</title>
-      </Helmet>
+    <HomeContext.Provider value={{ displaySignIn, switchMethod }}>
+      <div className="Home">
+        <Helmet>
+          <title>Login to your account at DineOut</title>
+        </Helmet>
 
-      <div className={classes.root}>
-            <Button
-              color="primary"
-              className={classes.button}
-              onClick={() => {
-                setDisplaySignIn(!displaySignIn);
-              }}
-              style={{ alignSelf: "center" }}
-            >
-              {displaySignIn ? "SIGN UP" : "LOGIN"}
-            </Button>
+        <div className={classes.root}>
+          <Button
+            color="primary"
+            className={classes.button}
+            onClick={() => {
+              setDisplaySignIn(!displaySignIn);
+            }}
+            style={{ alignSelf: "center" }}
+          >
+            {displaySignIn ? "SIGN UP" : "LOGIN"}
+          </Button>
+        </div>
 
+        {displaySignIn ? <LoginForm /> : <RegForm />}
       </div>
-
-      {displaySignIn ? <LoginForm /> : <RegForm />}
-    </div>
+    </HomeContext.Provider>
   );
 }
 
