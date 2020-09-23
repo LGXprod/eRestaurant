526416bbbdf3b148427c889056148198c9f7a815
Matthew <mattghanno@gmail.com>
2020-09-23 15:22:09 +1000

    ADD menu and barebones file upload form

---
+++ erestaurant-app/src/routes/Menu/Menu.js
@@ -0,0 +1,50 @@
+import React, { Fragment, useState } from "react";
+import { Button } from "@material-ui/core";
+import queryString from "querystring";
+
+function Menu() {
+  const [itemImg, setItemImg] = useState(null);
+
+  function uploadImg(e) {
+    setItemImg(e.target.files[0]);
+  }
+
+  async function submitNewItem(e) {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("img", itemImg);
+
+    const sendImg = await fetch("/Menu/Item", {
+      method: "POST",
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+      body: queryString.stringify({
+        itemName: "",
+        itemPrice: "",
+      }),
+    });
+
+    console.log(sendImg);
+  }
+
+  return (
+    <Fragment>
+      <h1>Work</h1>
+
+      <Button variant="contained" component="label">
+        Upload File
+        <input
+          type="file"
+          style={{ display: "none" }}
+          onChange={(e) => uploadImg(e)}
+        />
+      </Button>
+
+      <Button onChange={(e) => submitNewItem(e)}>Submit</Button>
+    </Fragment>
+  );
+}
+
+export default Menu;
