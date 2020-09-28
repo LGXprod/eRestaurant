39b6a76665acbdced6dcfe682c045327e79a08d4
LGXprod <mattghanno@gmail.com>
2020-09-28 14:59:32 +1000

    UPDATE changed routing configuration for menu config pag

--- erestaurant-app/src/index.js
+++ erestaurant-app/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home, Registration, Menu } from "./routes/Routes";
+import { Home, Registration, MenuConfig, Menu } from "./routes/Routes";
 
 ReactDOM.render(
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/Registration" component={Registration} />
-      <Route path="/Menu/Configuration" component={Menu} />
+      <Route path="/Menu" component={Menu} />
+      <Route path="/Menu/Configuration" component={MenuConfig} />
     </Switch>
   </Router>,
   document.getElementById('root')
--- erestaurant-app/src/routes/Routes.js
+++ erestaurant-app/src/routes/Routes.js
@@ -1,3 +1,4 @@
 export { default as Home } from "./Home/Home";
 export { default as Registration } from "./Home/components/RegForm";
 export { default as Menu } from "./Menu/Menu";
+export { default as MenuConfig } from "./Menu/MenuConfig";
